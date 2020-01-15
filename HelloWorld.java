868384f54a670bda10cf74ee8c1bbe99d1ab05be
Wade Helquist <ZWhiteShadow@Yahoo.com>
2020-01-14 19:20:44 -0700

    First Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.zwhiteshadow.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String message = "Hello World!";
+	    System.out.println(message);
+	}
+	
+}
